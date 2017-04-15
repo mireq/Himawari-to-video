3214e2c725e0100e3cf2e9159873b9ee10a5c041
Miroslav Bendík <miroslav.bendik@gmail.com>
2017-04-15 19:10:49 +0200

    Fix

--- make_4k.sh
+++ make_4k.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-python main.py --start-date 2017-01-01 --frames 1500 --resolution 4 -- -y -r 25 -crf 22 -c:v libx264 -pix_fmt yuv420p -preset placebo -level 5.0 -vf crop=2160:2200:20:20,pad=4096:2160:968:0
+python main.py --start-date 2017-01-01 --frames 1500 --resolution 4 -- -y -r 25 -crf 22 -c:v libx264 -pix_fmt yuv420p -preset placebo -level 5.0 -vf crop=2200:2160:20:20,pad=4096:2160:968:0
