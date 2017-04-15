f14a47dabee1981c6e40a5d89a7a545501305b6c
Miroslav Bendík <miroslav.bendik@gmail.com>
2017-04-15 19:28:22 +0200

    Added detection for empty frames

--- himawaritovideo/downloader.py
+++ himawaritovideo/downloader.py
@@ -1,13 +1,18 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+import os
 from datetime import timedelta
 from multiprocessing.dummy import Pool as ThreadPool
 
 import requests
-from PIL import Image
+from PIL import Image, ImageChops
 
 from .config import get_config
+
+
+class EmptyImageException(Exception):
+	pass
 
 
 class Downloader(object):
@@ -19,6 +24,7 @@ class Downloader(object):
 		self.config = get_config()
 		self.start_date = self.config.start_date
 		self.downloader_pool = ThreadPool(processes=self.POOL_SIZE)
+		self.empty_image = Image.open(open(os.path.join(os.path.dirname(__file__), 'empty.png'), 'rb'), 'r')
 
 	def get_urls(self, frame_date):
 		url_template = 'http://himawari8-dl.nict.go.jp/himawari8/img/D531106/{resolution}d/550/{year}/{month}/{day}/{hour}{minute}00_{x}_{y}.png'
@@ -39,19 +45,28 @@ class Downloader(object):
 				yield url_template.format(**context)
 
 	def compose_image(self, urls):
+		urls = list(urls)
 		s = self.IMAGE_RESOLUTION * self.config.resolution
 		im = Image.new('RGB', (s, s))
 		try:
 			responses = self.downloader_pool.map(lambda url: requests.get(url, stream=True), urls)
 			for i, response in enumerate(responses):
 				part = Image.open(response.raw)
+				if ImageChops.difference(self.empty_image, part).getbbox() is None:
+					raise EmptyImageException()
 				im.paste(part, ((i // self.config.resolution) * self.IMAGE_RESOLUTION, (i % self.config.resolution) * self.IMAGE_RESOLUTION))
+		except EmptyImageException:
+			return None
 		except Exception as e:
 			print(e)
-			return im
+			return None
+		print(urls)
+		im.save('/dev/shm/test.png')
 		return im
 
 	def __iter__(self):
 		for i in range(self.config.frames):
 			frame_date = self.start_date + timedelta(seconds=i*self.SECONDS_BETWEEN_FRAMES)
-			yield self.compose_image(self.get_urls(frame_date))
+			img = self.compose_image(self.get_urls(frame_date))
+			if img is not None:
+				yield img
